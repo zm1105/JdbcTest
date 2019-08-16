563a3a448bcc4a1a6a8db57ed77da8df4a28ae48
zm1105 <827556302@qq.com>
2019-08-16 18:32:53 +0800

    Jdbc入门  update和select初级

---
+++ src/main/java/Jdbc/JdbcDemo01.java
@@ -0,0 +1,33 @@
+package Jdbc;
+
+import java.sql.*;
+
+public class JdbcDemo01 {
+    public static void main(String[] args) throws SQLException {
+        //注册驱动
+        java.sql.DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+        //Class.forName("com.mysql.cj.jdbc");
+        //获取数据库连接对象
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/zhoumin?" +
+                        "serverTimezone=UTC&" +
+                        "useUnicode=true&characterEncoding=utf8&useSSL=false",
+                "zhoumin", "qweasd12345");
+        /** //定义sql
+         //String sql = "update user set password=222222 where id=2";
+         //获取执行对象
+         Statement statement = connection.createStatement();
+         //执行sql
+         int i = statement.executeUpdate(sql);
+         System.out.println(i);**/
+
+        String sql1 = "select * from user where id=2";
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql1);
+        while (resultSet.next()) {
+            String name = resultSet.getString("username");
+            System.out.println(name);
+        }
+        statement.close();
+        connection.close();
+    }
+}
