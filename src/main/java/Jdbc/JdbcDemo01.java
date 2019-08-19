1f5939d9f953167289eb2e889bc907745821b62e
zm1105 <827556302@qq.com>
2019-08-19 17:08:54 +0800

    修改

--- src/main/java/Jdbc/JdbcDemo01.java
+++ src/main/java/Jdbc/JdbcDemo01.java
@@ -3,7 +3,7 @@ package Jdbc;
 import java.sql.*;
 
 public class JdbcDemo01 {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         //注册驱动
         java.sql.DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
         //Class.forName("com.mysql.cj.jdbc");
@@ -12,21 +12,24 @@ public class JdbcDemo01 {
                         "serverTimezone=UTC&" +
                         "useUnicode=true&characterEncoding=utf8&useSSL=false",
                 "zhoumin", "qweasd12345");
-        /** //定义sql
-         //String sql = "update user set password=222222 where id=2";
-         //获取执行对象
+        /** 定义sql  update
+         String sql = "update user set password=222222 where id=2";
+         获取执行对象
          Statement statement = connection.createStatement();
-         //执行sql
+         执行sql
          int i = statement.executeUpdate(sql);
          System.out.println(i);**/
 
+        //select
         String sql1 = "select * from user where id=2";
         Statement statement = connection.createStatement();
+        //结果集对象
         ResultSet resultSet = statement.executeQuery(sql1);
         while (resultSet.next()) {
             String name = resultSet.getString("username");
             System.out.println(name);
         }
+        //释放资源
         statement.close();
         connection.close();
     }
