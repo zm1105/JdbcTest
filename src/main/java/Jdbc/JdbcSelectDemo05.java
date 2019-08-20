ac9770a9f32ff3d3f90a0a0d680e802704ea86bb
zm1105 <827556302@qq.com>
2019-08-20 15:56:37 +0800

    登录

---
+++ src/main/java/Jdbc/JdbcSelectDemo05.java
@@ -0,0 +1,79 @@
+package Jdbc;
+
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class JdbcSelectDemo05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入用户名");
+        String username = scanner.nextLine();
+        System.out.println("请输入密码");
+        String password = scanner.nextLine();
+        boolean flag = new JdbcSelectDemo05().Login(username, password);
+        if (flag) {
+            System.out.println("登录成功");
+        } else {
+            System.out.println("用户名或密码错误");
+        }
+    }
+
+
+    public boolean Login(String usurname, String password) {
+        if (usurname == null || password == null) {
+            return false;
+        }
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            DriverManager.registerDriver(new Driver());
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/zhoumin?" +
+                            "serverTimezone=UTC&" +
+                            "useUnicode=true&characterEncoding=utf8&useSSL=false",
+                    "zhoumin", "qweasd12345");
+            statement = connection.createStatement();
+            String sql = "select * from user where username=? and password=?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, usurname);
+            preparedStatement.setString(2, password);
+            resultSet = preparedStatement.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
+    }
+}
